26f43e431aa00e09c40e8a0e1c04f0a745354489
KonstiDerCharmeur <74175473+KonstiDerCharmeur@users.noreply.github.com>
2025-11-14 23:29:11 +0100

    dash is drinn guuuuuys

--- Unity/Assets/Scripts/PlayerScripts/PlayerMove.cs
+++ Unity/Assets/Scripts/PlayerScripts/PlayerMove.cs
@@ -8,14 +8,26 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] float speed;
     [SerializeField] float jumpHeight;
     [SerializeField] private InputActionReference jump;
+    [SerializeField] private InputActionReference dash;
+
+    [SerializeField] public float gravity;
     Vector2 movement = new Vector2();
     public Rigidbody2D rb;
     bool isGrounded;
+
+    public Vector3 moveDirection;
+    public float maxDashTime = 1.0f;
+    [SerializeField] public float dashSpeed = 5.0f;
+    public float dashStoppingSpeed = 0.1f;
+
+    private float currentDashTime;
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
 
         jump.action.performed += OnJump;
+        dash.action.performed += OnDash;
+
 
     }
 
@@ -29,13 +41,29 @@ public class PlayerMovement : MonoBehaviour
         }
     }
 
+    private void OnDash(InputAction.CallbackContext context)
+    {
+        Debug.Log("We dash1.");
+        currentDashTime = 0.0f;
+    }
+
     private void Update()
     {
+        if (!isGrounded)
+        {
+            movement.y -= gravity;
+        }
         Vector2 position = transform.position;
-        position.x += movement.x * Time.deltaTime * speed;
+        float xMovement = movement.x * Time.deltaTime * speed;
+        if (currentDashTime < maxDashTime)
+        {
+            xMovement *= dashSpeed;
+            currentDashTime += dashStoppingSpeed;
+        }
+        
+        position.x += xMovement;
         position.y += movement.y * Time.deltaTime * jumpHeight;
         transform.position = position;
-
     }
 
     public void GetPlayerMovement(InputAction.CallbackContext context)
@@ -50,8 +78,6 @@ public class PlayerMovement : MonoBehaviour
             movement.y = 0;
         }
     }
-    public void GetPlayerDash(InputAction.CallbackContext context)
-    {
-        
-    }
+
+
 }
