36978d029306efb7bd9d71435d27fa81006deb10
Crafter1606 <167441275+Crafter1606@users.noreply.github.com>
2025-11-15 17:24:35 +0100

    Aktueller Stand für Konsti

--- Unity/Assets/Scripts/EnemyScripts/EnemyAttack.cs
+++ Unity/Assets/Scripts/EnemyScripts/EnemyAttack.cs
@@ -2,15 +2,32 @@ using UnityEngine;
 
 public class EnemyAttack : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    bool attackOnCooldown;
+    bool inRange;
+    float windUpTime;
+    float currentWindUpTime;
+
     void Start()
     {
-        
-    }
+        attackOnCooldown = true;
+        inRange = false;
+        currentWindUpTime = 0f;
+    } 
 
     // Update is called once per frame
     void Update()
     {
+        if (inRange)
+        {
+            currentWindUpTime++;
+        }
+
         
+    }
+
+    public void AttackRangeDetection()
+    {
+        Debug.Log("We are ready to attack!");
+        inRange = true;
     }
 }
