c12504ecc7119b09cc6bbf2b308bc821dad1a42b
Crafter1606 <167441275+Crafter1606@users.noreply.github.com>
2025-11-15 17:24:35 +0100

    Aktueller Stand für Konsti

--- Unity/Assets/Scripts/EnemyScripts/EnemyAttack.cs
+++ Unity/Assets/Scripts/EnemyScripts/EnemyAttack.cs
@@ -2,8 +2,6 @@ using UnityEngine;
 
 public class EnemyAttack : MonoBehaviour
 {
-<<<<<<< HEAD
-    bool DamageAreaActive;
     bool attackOnCooldown;
     bool inRange;
     float windUpTime;
@@ -14,45 +12,21 @@ public class EnemyAttack : MonoBehaviour
         attackOnCooldown = true;
         inRange = false;
         currentWindUpTime = 0f;
-        DamageAreaActive = false;
-        gameObject.SetActive(DamageAreaActive);
-    } 
-=======
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
     }
->>>>>>> 63f1b3eb (Adding Health and Attack Tags and things)
 
     // Update is called once per frame
     void Update()
     {
-<<<<<<< HEAD
         if (inRange)
         {
             currentWindUpTime++;
         }
 
-        if (currentWindUpTime >= windUpTime)
-        {
-            DoAttack();
-        }
     }
 
     public void AttackRangeDetection()
     {
         Debug.Log("We are ready to attack!");
         inRange = true;
-    }
-
-    public void DoAttack()
-    {
-        DamageAreaActive = true;
-        gameObject.SetActive(DamageAreaActive);
-        DamageAreaActive = false;
-=======
-        
->>>>>>> 63f1b3eb (Adding Health and Attack Tags and things)
     }
 }
