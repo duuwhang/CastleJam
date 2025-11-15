9b8a72c7a94a9903251ef6a83cff982f2b44f207
Crafter1606 <167441275+Crafter1606@users.noreply.github.com>
2025-11-15 03:17:27 +0100

    Monster Kreiert
    Ja also Chasing funktioniert jetzt als Event das Invoked wird wenn der Player es triggert sonst aber nichts und das ganze teleportiert sich dann spontan auf deinen arsch und fängt an zu buggen is doch geil

--- Unity/Assets/Scripts/EnemyScripts/EnemyMovement.cs
+++ Unity/Assets/Scripts/EnemyScripts/EnemyMovement.cs
@@ -17,6 +17,9 @@ public class EnemyMovement : MonoBehaviour
     int direction = 1;
     bool collided;
     Transform playerLocation;
+    bool huntState;
+    [SerializeField] int aggroTime;
+    int aggroCounter = 0;
 
 
     void Start()
@@ -24,10 +27,12 @@ public class EnemyMovement : MonoBehaviour
         float currentMoveDistance = distance;
 
         playerLocation = GameObject.FindWithTag("Player").transform;
+        huntState = false;
     }
     void Update()
     {
         // --- Patrol Movement ---
+        if (huntState) return;
         currentMoveDistance = currentMoveDistance + 1 * Time.deltaTime * multiplier;
         UnityEngine.Vector2 position =  transform.position;
         position += moveVector * speed * direction * Time.deltaTime;
@@ -52,25 +57,33 @@ public class EnemyMovement : MonoBehaviour
     }
     public void TurnAround()
     {
-        Debug.Log("Wir gehen in Turn Around");
         // --- Das für Wall Turning ---
         if (collided)
         {
-            Debug.Log("Wir Colliden mit Wall");
             direction = direction * negative;
             currentMoveDistance = moveDistance - currentMoveDistance;
         }
     }
     public void ChasePlayer()
     {
+        // --- Check if it is actually the Player because generalized one does not do that ---
+        // --- Or we fuck it and make one specifically for the Player because I do not understand it ---
+        Debug.Log("The Hunt is on!");
         if (playerLocation == null) return;
-
+        huntState = true;
+        while (aggroCounter < aggroTime)
+        {
+        Debug.Log("We chasing now");
         UnityEngine.Vector3 direction = playerLocation.position - transform.position;
         direction.y = 0;
-
+        
         direction = direction.normalized;
 
         transform.position += direction * speed * Time.deltaTime;
+        aggroCounter++;
+        }
+        huntState = false;
+        aggroCounter = 0;
     }
     
 }
