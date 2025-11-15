aecef3fd705cc34438778b9ea2a4f4ac80bb6889
Crafter1606 <167441275+Crafter1606@users.noreply.github.com>
2025-11-15 16:18:46 +0100

    Adding Health and Attack Tags and things

---
+++ Unity/Assets/Scripts/EnemyScripts/WeaponCollision.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class WeaponCollisionDetect : MonoBehaviour
+{
+    public UnityEvent WeaponDetection;
+
+    public void Start()
+    {
+        if (WeaponDetection == null)
+            WeaponDetection = new UnityEvent();
+    }
+
+
+    void OnTriggerEnter2D(Collider2D PlayerDetector)
+    {
+    if (PlayerDetector.CompareTag("Weapon"))
+        {
+            WeaponDetection.Invoke();
+        }
+    }
+}
